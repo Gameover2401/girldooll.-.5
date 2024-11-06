25f30de0ad127d851ad15320642b2619fc831cda
АДМИН <@DESKTOP-S9U5IDE>
2024-11-06 22:41:06 +0300

    Добавьте файлы проекта.

---
+++ girldooll.ПР.5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение x (0<=x <1) = ");
+double x=double.Parse(Console.ReadLine());
+if (x < 0 || x >= 1)
+{
+    Console.WriteLine("Ошибка, x должно быть в диапозоне [0,1). ");
+    return;
+}
+double f = 1 / (1 - x);
+Console.WriteLine($"f(x)=1/(1-x)={f}");
+
+double i = 1;
+int d = 10;
+for (int n=0; n<d; n++)
+{
+
+    i *= (1 + Math.Pow(x, Math.Pow(2, n)));
+}
+Console.WriteLine($"Эвивалентное произведение(первые {d} членов) : {i}");
